7636a5b84a38f40f28553bbe7fc44c25eca4bf2f
IgorKovale <155283623+IgorKovale@users.noreply.github.com>
2025-04-25 15:02:04 +0300

    Update visit_duration.py

--- datacenter/visit_duration.py
+++ datacenter/visit_duration.py
@@ -5,16 +5,16 @@ def get_duration(visit):
     time_now = localtime()
     enter_localtime = localtime(visit.entered_at)
     if not visit.leaved_at:
-        duration = time_now-enter_localtime
+        duration = time_now - enter_localtime
     else:
-        leaved_localtime=localtime(visit.leaved_at)
-        duration = leaved_localtime-enter_localtime
+        leaved_localtime = localtime(visit.leaved_at)
+        duration = leaved_localtime - enter_localtime
     return duration
 
 def format_duration(duration):
     total_seconds = duration.total_seconds()
     seconds_in_hour = 3600
-    seconds_in_minute=60
+    seconds_in_minute = 60
     hours = total_seconds // seconds_in_hour
     minutes = (total_seconds % seconds_in_hour) // seconds_in_minute
     seconds = (total_seconds % seconds_in_hour) % seconds_in_minute
@@ -22,5 +22,6 @@ def format_duration(duration):
 
 def is_visit_long(visit, minutes=60):
     duration = get_duration(visit)
-    duration_minutes = duration.total_seconds()/60
+    seconds_in_minute = 60
+    duration_minutes = duration.total_seconds() / seconds_in_minute
     return duration_minutes > minutes
